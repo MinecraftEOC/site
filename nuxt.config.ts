23b0461657a0d43a7f0084291e0635c92e70951d
Daniil Pinov <d.pinov@samolet.ru>
2026-07-22 21:36:30 +0700

    feat: настроены модули иконок, шрифтов и линта

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,38 @@ export default defineNuxtConfig({
     },
     modules: [
         '@nuxt/eslint',
+        '@nuxt/fonts',
+        '@nuxt/icon',
         '@pinia/nuxt',
     ],
+    icon: {
+        serverBundle: 'local',
+    },
+    fonts: {
+        // Шрифты самохостятся; тянем только реально используемые веса
+        // и кириллицу с латиницей (см. дизайн-систему, раздел 2).
+        defaults: {
+            subsets: ['cyrillic', 'latin'],
+        },
+        families: [
+            { name: 'Alegreya', weights: [700, 800] },
+            { name: 'Alegreya Sans', weights: [400, 500] },
+            { name: 'JetBrains Mono', weights: [400] },
+        ],
+    },
+    components: [
+        { path: '~/components/ui', pathPrefix: false },
+        '~/components',
+    ],
+    css: ['~/assets/scss/default.scss'],
     vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@use "~/assets/scss/variables" as *;\n@use "~/assets/scss/mixins" as *;\n',
+                },
+            },
+        },
         server: {
             allowedHosts: ['eoc-rp.ru', 'www.eoc-rp.ru'],
         },
